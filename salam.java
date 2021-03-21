e7a5ff7a71154c9e37b9064fb8bddca980c411eb
simo684 <58590105+simo684@users.noreply.github.com>
2021-03-21 11:39:50 +0100

    Update salam.java

--- salam.java
+++ salam.java
@@ -9,7 +9,12 @@ public class hello {
 
 
 	public void showsalam() {
-		
+		System.out.println("hello tout le monde");
 		System.out.println("je t ai ecris from github");
+
 		System.out.println("attension");
 	}
+
+		
+	}
+
